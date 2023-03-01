031687ca841c7706a76d1c0a6fc3906a5c0b67f4
shruti166 <shruti.chemistry.lm@gmail.com>
2023-03-01 17:49:08 +0530

    created custom logger

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connect = require('./configs/db.js');
-const exerciseRouter = require('./routes/exerciseRouter.js');
+const connect = require("./configs/db.js");
+const logger = require("./middlewear/logger.js");
+const exerciseRouter = require("./routes/exerciseRouter.js");
 
 app.use(express.json());
+
+app.use(logger);
 
 app.use("/exercise", exerciseRouter);
 
 const port = 8080;
 connect();
 app.listen(port, () => {
-    console.log(`server listening on ${port}`);
-})
+  console.log(`server listening on ${port}`);
+});
