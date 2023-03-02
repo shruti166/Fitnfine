061b86901b82d2e2cca6133e575a2839fd0f417f
shruti166 <shruti.chemistry.lm@gmail.com>
2023-03-02 23:46:48 +0530

    added

--- server/controllers/exercise.js
+++ server/controllers/exercise.js
@@ -1,58 +1,64 @@
 const Exercises = require("../models/ExerciseSchema");
-const asyncHandler = require('../middlewear/async')
+const asyncHandler = require("../middlewear/async");
 // @desc     Get all Excercises
 // @route    GET /exercise/
 // @access   Public
 
 exports.getExercise = asyncHandler(async (req, res, next) => {
-  
-    const exercise = await Exercises.find();
-    res.status(200).json({ success: true, data: exercise });
-});
+  let query;
 
-// Pagination
+  let queryStr = JSON.stringify(req.query);
 
-const page = parseInt(req.query.page, 10) || 1;
-const limit = parseInt(req.query.limit, 10) || 1;
-const startIndex = (page - 1)*limit;
-const endIndex = page*limit;
-const total = await Exercises.countDocuments();
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  console.log(queryStr);
 
-query = query.skip(startIndex).limit(limit);
+  query = Exercises.find(JSON.parse(queryStr));
 
-// Excecuting query
-const exercise = await query;
+  // Pagination
 
-//Paginantion result 
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Exercises.countDocuments();
 
-const pagination = {};
+  query = query.skip(startIndex).limit(limit);
 
-if(endIndex < total) {
+  // Excecuting query
+  const exercise = await query;
+
+  //Paginantion result
+
+  const pagination = {};
+
+  if (endIndex < total) {
     pagination.next = {
-        page: page + 1,
-        limit
-    }
-}
+      page: page + 1,
+      limit,
+    };
+  }
 
-if(startIndex > 0) {
+  if (startIndex > 0) {
     pagination.prev = {
-        page: page - 1,
-        limit
-    }
-}
+      page: page - 1,
+      limit,
+    };
+  }
 
-
-
+  res.status(200).json({ success: true, data: exercise });
+});
 
 // @desc     Get single Excercises
 // @route    GET /exercise/:id
 // @access   Public
 
 exports.getSingleExercise = asyncHandler(async (req, res, next) => {
-    const exercise = await Exercises.findById(req.params.id);
+  const exercise = await Exercises.findById(req.params.id);
 
-    res.status(200).json({ success: true, data: exercise });
-  
+  res.status(200).json({ success: true, data: exercise });
 });
 
 // @desc     Create new  Excercise
@@ -60,8 +66,6 @@ exports.getSingleExercise = asyncHandler(async (req, res, next) => {
 // @access   Private
 
 exports.createExercise = asyncHandler(async (req, res, next) => {
-  
-    const exercise = await Exercises.create(req.body);
-    res.status(201).json({ success: true, data: exercise });
-  
+  const exercise = await Exercises.create(req.body);
+  res.status(201).json({ success: true, data: exercise });
 });
